67845b838c472e595994d8d628b2870207209c60
Yavuz Tas <yavuzslmtas@gmail.com>
2019-07-15 16:34:24 +0200

    add application-context samples

---
+++ src/main/java/dev/yavuztas/spring/SpringFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package dev.yavuztas.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsApplication.class, args);
+	}
+
+}
